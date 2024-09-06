6c6425cc4246ade37bffab969baa92d07150f6ef
FurkanAndac <furkan_andac_@hotmail.com>
2024-09-06 13:25:52 +0200

    add next-contentlayer to package.json

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -1,6 +1,5 @@
-import { defineDocumentType, makeSource } from 'contentlayer/source-files';
+import { defineDocumentType, makeSource } from '@contentlayer/source-files'
 
-// Example of defining a document type
 const Post = defineDocumentType(() => ({
   name: 'Post',
   filePathPattern: `**/*.md`,
@@ -8,10 +7,9 @@ const Post = defineDocumentType(() => ({
     title: { type: 'string', required: true },
     date: { type: 'date', required: true },
   },
-}));
+}))
 
-// Source configuration for contentlayer
 export default makeSource({
   contentDirPath: 'content',
   documentTypes: [Post],
-});
+})
